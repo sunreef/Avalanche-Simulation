179482550987f824cebdbc1f69d71aa844ed1f40
Victor Cornillère <v.cornillere@gmail.com>
2016-11-11 18:36:18 +0100

    Replace GLUT with GLFW and improved event handling

---
+++ src/event_processing.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <cmath>
+#include <algorithm>
+
+#define MY_PI 3.14159265359f
+
+
+
+class EventProcessing {
+public:
+	int size_x;
+	int size_y;
+
+
+private:
+	float speed = 12.0f;
+	float mouseSpeed = 0.1f;
+	float zoomSpeed = 5.0f;
+
+	float deltaTime;
+	clock_t time;
+
+	int x, y;
+	int dx, dy;
+
+	bool keys[349];
+	bool mouse_buttons[8];
+
+	glm::vec3 position;
+	float horizontalAngle, verticalAngle;
+
+	float fov;
+
+public:
+	EventProcessing() {
+		deltaTime = 0; 
+		time = clock();
+		x = 0;
+		y = 0;
+		dx = 0;
+		dy = 0;
+
+		for (int i = 0; i < 349; i++) {
+			keys[i] = false;
+		}
+		for (int i = 0; i < 8; i++) {
+			mouse_buttons[i] = false;
+		}
+
+		position = glm::vec3(0, 0, 0);
+		horizontalAngle = MY_PI;
+		verticalAngle = 0.0f;
+
+		fov = 45.0f;
+	}
+
+	void initHandler(GLFWwindow* window) {
+		glfwGetWindowSize(window, &size_x, &size_y);
+	}
+
+	void mouseMotionCallback(GLFWwindow* window, double x_pos, double y_pos) {
+		dx = x_pos - x;
+		dy = y_pos - y;
+		x = x_pos;
+		y = y_pos;
+	}
+
+	void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+		if (action == GLFW_PRESS) {
+			mouse_buttons[button] = true;
+		}
+		if (action == GLFW_RELEASE) {
+			mouse_buttons[button] = false;
+		}
+	}
+
+	void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+		if (action == GLFW_PRESS) {
+			keys[key] = true;
+		}
+		if (action == GLFW_RELEASE) {
+			keys[key] = false;
+		}
+	}
+
+	void scrollCallback(GLFWwindow* window, double x_offset, double y_offset) {
+		fov -= zoomSpeed * y_offset;
+
+		if (fov < 10) {
+			fov = 10.0f;
+		}
+		if (fov > 170) {
+			fov = 170.0f;
+		}
+	}
+
+	bool processEvents(glm::mat4 &view, glm::mat4 &proj) {
+		deltaTime = (float)(clock() - time) / CLOCKS_PER_SEC;
+		time = clock();
+
+		if (mouse_buttons[GLFW_MOUSE_BUTTON_1]) {
+			horizontalAngle -= mouseSpeed * std::min(deltaTime, 0.03f) * dx;
+			verticalAngle += mouseSpeed *  std::min(deltaTime, 0.03f) * dy;
+			dx = 0;
+			dy = 0;
+		}
+
+		glm::vec3 direction = glm::vec3(
+			cos(verticalAngle) * sin(horizontalAngle),
+			cos(verticalAngle) * cos(horizontalAngle),
+			sin(verticalAngle)
+		);
+
+		glm::vec3 left = glm::vec3(
+			sin(horizontalAngle - 3.14f / 2.0f),
+			cos(horizontalAngle - 3.14f / 2.0f),
+			0
+		);
+
+		glm::vec3 up = -glm::cross(left, direction);
+
+		if (keys[GLFW_KEY_W]) {
+			position += deltaTime * speed * direction;
+		}
+		if (keys[GLFW_KEY_S]) {
+			position -= deltaTime * speed *direction;
+		}
+		if (keys[GLFW_KEY_A]) {
+			position += deltaTime * speed * left;
+		}
+		if (keys[GLFW_KEY_D]) {
+			position -= deltaTime * speed * left;
+		}
+		if (keys[GLFW_KEY_R]) {
+			position += deltaTime * speed * up;
+		}
+		if (keys[GLFW_KEY_F]) {
+			position -= deltaTime * speed * up;
+		}
+		if (keys[GLFW_KEY_ESCAPE]) {
+			glfwTerminate();
+			return false;
+		}
+
+		view = glm::lookAt(position, position + direction, up);
+
+		proj = glm::perspective(glm::radians(fov), (float)size_x / size_y, 0.01f, 1000.0f);
+
+		return true;
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 
 #include <GL/glew.h>
 
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <GLUT/glut.h>
-
-//#include <GL/glut.h>
-#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/rotate_vector.hpp>
 
 #include <time.h>
 #include <chrono>
@@ -21,166 +15,76 @@
 
 #include "mesh.h"
 #include "program.h"
-#include "callback_struct.h"
+#include "event_processing.h"
 
-#define MY_PI 3.14159265359f
 
-int size_x = 1920;
-int size_y = 1080;
+EventProcessing eventHandler;
 
-float azerty_rules = true;
-
-CallbackStruct callbackStructure;
-
-void display() {}
-
-void mouseCallback(int button, int state, int x, int y) {
-	callbackStructure.x = x;
-	callbackStructure.y = y;
-
-	callbackStructure.mouse_buttons[button] = (state == GLUT_DOWN);
+void mouseMotionCallback(GLFWwindow* window, double x_pos, double y_pos) {
+	eventHandler.mouseMotionCallback(window, x_pos, y_pos);
 }
 
-void passiveMotionCallback(int x, int y) {
-	callbackStructure.dx = 0;
-	callbackStructure.dy = 0;
-
-	callbackStructure.x = x;
-	callbackStructure.y = y;
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+	eventHandler.mouseButtonCallback(window, button, action, mods);
 }
 
-void motionCallback(int x, int y) {
-	callbackStructure.dx = x - callbackStructure.x;
-	callbackStructure.dy = y - callbackStructure.y;
-	callbackStructure.x = x;
-	callbackStructure.y = y;
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	eventHandler.keyboardCallback(window, key, scancode, action, mods);
 }
 
-void keyboardCallback(unsigned char key, int x, int y) {
-	callbackStructure.ascii_keys[key] = true;
-}
-
-void keyboardUpCallback(unsigned char key, int x, int y) {
-	callbackStructure.ascii_keys[key] = false;
-}
-
-void initOpenGLContext(int argc, char** argv) {
-
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
-  glutInitContextVersion(4,1);
-	glutSetOption(GLUT_MULTISAMPLE, 16);
-	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(size_x, size_y);
-	glutCreateWindow("Hello OpenGL!");
-
-
-	glutDisplayFunc(display);
-	glutMouseFunc(mouseCallback);
-	glutMotionFunc(motionCallback);
-	glutPassiveMotionFunc(passiveMotionCallback);
-	glutKeyboardFunc(keyboardCallback);
-	glutKeyboardUpFunc(keyboardUpCallback);
-
-	glewInit();
+void scrollCallback(GLFWwindow* window, double x_offset, double y_offset) {
+	eventHandler.scrollCallback(window, x_offset, y_offset);
 }
 
 int main(int argc, char** argv) {
 
-	initOpenGLContext(argc, argv);
+	glfwInit();
+
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_SAMPLES, 16);
+
+	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+	GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Avalanche simulation", monitor, NULL);
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetKeyCallback(window, keyboardCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetCursorPosCallback(window, mouseMotionCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+
+	eventHandler.initHandler(window);
+
+	glewInit();
 
 	Program prog("../src/shaders/basic_shading.vert", "../src/shaders/basic_shading.frag");
 
 	MeshAsset mesh_asset(std::string("../data/meshes/plane.obj"));
-
 	MeshAsset particle_asset(std::string("../data/meshes/sphere.obj"));
 
 	MeshInstance instance_of_mesh(&mesh_asset);
-
-
 	std::vector<MeshInstance> particles;
 
-	for (int p = 0; p < 100000; p++) {
-
+	for (int p = 0; p < 10000; p++) {
 		float x = (float)(rand() % 5000) / 1000;
 		float y = (float)(rand() % 5000) / 1000;
 		float z = (float)(rand() % 5000) / 1000;
-
-		particles.push_back(MeshInstance(&particle_asset, glm::vec3(x, y, z), glm::vec3(0,0,0), 0.05));
+		particles.push_back(MeshInstance(&particle_asset, glm::vec3(x, y, z), glm::vec3(0, 0, 0), 0.05));
 	}
 
-
-	float horizontalAngle = MY_PI;
-	float verticalAngle = 0.0f;
-	float initialFOV = 45.0f;
-
-	float speed = 10.0f;
-	float mouseSpeed = 0.1f;
-
-	clock_t time = clock();
-	float deltaTime = 0;
-
-	int frameTime = CLOCKS_PER_SEC / 60;
-	glm::vec3 position = glm::vec3(7, 10, 3.0);
-	glm::vec3 direction;
-	glm::vec3 left;
-	glm::vec3 up;
 	glm::mat4 view;
-	glm::mat4 proj;
+	glm::mat4 proj /*= glm::perspective(initialFOV, (float)size_x / size_y, 0.01f, 1000.0f)*/;
 
 	glEnable(GL_DEPTH_TEST);
 	glDisable(GL_CULL_FACE);
 	while (true) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		{
-			glutMainLoopEvent();
-			deltaTime = (float)(clock() - time) / CLOCKS_PER_SEC;
-			time = clock();
-			horizontalAngle -= mouseSpeed * std::min(deltaTime, 0.03f) * callbackStructure.dx;
-			callbackStructure.dx = 0;
-			verticalAngle += mouseSpeed *  std::min(deltaTime, 0.03f) * callbackStructure.dy;
-			callbackStructure.dy = 0;
+		glfwPollEvents();
+		bool keep_rendering = eventHandler.processEvents(view, proj);
 
-			direction = glm::vec3(
-				cos(verticalAngle) * sin(horizontalAngle),
-				cos(verticalAngle) * cos(horizontalAngle),
-				sin(verticalAngle)
-			);
-
-			left = glm::vec3(
-				sin(horizontalAngle - 3.14f / 2.0f),
-				cos(horizontalAngle - 3.14f / 2.0f),
-				0
-			);
-
-			up = -glm::cross(left, direction);
-
-			if (callbackStructure.ascii_keys[(azerty_rules) ? 'z' : 'w']) {
-				position += deltaTime * speed * direction;
-			}
-			if (callbackStructure.ascii_keys['s']) {
-				position -= deltaTime * speed *direction;
-			}
-			if (callbackStructure.ascii_keys['d']) {
-				position -= deltaTime * speed * left;
-			}
-			if (callbackStructure.ascii_keys[(azerty_rules) ? 'q' : 'a']) {
-				position += deltaTime * speed * left;
-			}
-			if (callbackStructure.ascii_keys['r']) {
-				position += deltaTime * speed * up;
-			}
-			if (callbackStructure.ascii_keys['f']) {
-				position -= deltaTime * speed * up;
-			}
-			if (callbackStructure.ascii_keys[27]) {
-				break;
-			}
-		}
-		view = glm::lookAt(position, position + direction, up);
-		proj = glm::perspective(initialFOV, (float)size_x / size_y, 0.01f, 1000.0f);
-	
 		prog.useProgram();
 
 		prog.loadProjMatrix(proj);
@@ -190,10 +94,15 @@ int main(int argc, char** argv) {
 			instance.draw(prog, view);
 		}
 		prog.stopUseProgram();
-		glutSwapBuffers();
+		glfwSwapBuffers(window);
+
+		if (!keep_rendering) {
+			break;
+		}
 	}
 
 	prog.destroy();
 	mesh_asset.destroy();
+	particle_asset.destroy();
 	return 0;
 }
